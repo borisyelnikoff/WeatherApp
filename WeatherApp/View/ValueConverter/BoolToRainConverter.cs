13113d97c73cffdc79a547e72faf1a73271a0998
Bart Zawada <b.zawada84@gmail.com>
2023-12-18 11:41:05 +0100

    Add BoolToRainingConverter.

---
+++ WeatherApp/View/ValueConverter/BoolToRainConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WeatherApp.View.ValueConverter
+{
+    public class BoolToRainConverter : IValueConverter
+    {
+        private const string IsRainingMessage = "Currently raining";
+        private const string IsNotRainingMessage = "Currently not raining";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isRaining = (bool)value;
+            if (isRaining) 
+            {
+                return IsRainingMessage;
+            }
+
+            return IsNotRainingMessage;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isRaining = value as string;
+            if (isRaining == IsRainingMessage)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
